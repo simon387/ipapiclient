34306b7306211b2061b494c7acc242a001491714
Simone Celia <simone.celia@simonecelia.it>
2025-03-06 11:33:37 +0100

    1.0.1

--- src/main/java/it.simonecelia.ipapiclient/RestApiCaller.java
+++ src/main/java/it.simonecelia.ipapiclient/RestApiCaller.java
@@ -13,7 +13,9 @@ public class RestApiCaller {
 
 	private static final String ENDPOINT = "https://www.simonecelia.it/ipapi/be/ip/update.php";
 
-	private static final String PAYLOAD = "{\"key\": \"value\"}";  // Dati da inviare nel corpo della richiesta PUT (se necessario)
+	private static final String PAYLOAD = "{\"key\": \"value\"}";  // useless
+
+	private static final int MINUTES = 10;
 
 	public static void main ( String[] args ) {
 		Executors.newSingleThreadScheduledExecutor ().scheduleAtFixedRate ( () -> {
@@ -22,42 +24,26 @@ public class RestApiCaller {
 			} catch ( Exception e ) {
 				System.err.println ( "Errore durante la chiamata: " + e.getMessage () );
 			}
-		}, 0, 1, TimeUnit.MINUTES );  // Inizia subito e ripete ogni minuto
+		}, 0, MINUTES, TimeUnit.MINUTES );
 	}
 
 	private static void sendPutRequest () throws Exception {
-		// Creazione della URL
 		var connection = getHttpURLConnection ();
-
-		// Scrivi il payload nel corpo della richiesta
 		try ( var os = connection.getOutputStream () ) {
 			var input = RestApiCaller.PAYLOAD.getBytes ( StandardCharsets.UTF_8 );
 			os.write ( input, 0, input.length );
 		}
-
-		// Ottieni il codice di risposta
 		var responseCode = connection.getResponseCode ();
-		System.out.println ( "Codice risposta: " + responseCode );
-
-		// Puoi gestire la risposta se necessario (ad esempio leggere il corpo della risposta)
-		if ( responseCode == HttpURLConnection.HTTP_OK ) {
-			System.out.println ( "Richiesta eseguita correttamente." );
-		} else {
-			System.out.println ( "Errore durante l'esecuzione della richiesta." );
-		}
+		System.out.println ( "Response code: " + responseCode );
 	}
 
 	private static HttpURLConnection getHttpURLConnection () throws URISyntaxException, IOException {
 		var uri = new URI ( ENDPOINT );
-		// Crea la connessione utilizzando la URI
 		var url = uri.toURL ();
-		// Apri la connessione
 		var connection = (HttpURLConnection) url.openConnection ();
-
-		// Imposta il metodo HTTP come PUT
 		connection.setRequestMethod ( "PUT" );
 		connection.setRequestProperty ( "Content-Type", "application/json" );
-		connection.setDoOutput ( true );  // Indica che invieremo dati nel corpo della richiesta
+		connection.setDoOutput ( false );  // true, send data in request's body
 		return connection;
 	}
 }
