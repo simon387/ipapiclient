99844def8110f33e885bc6cd0faf852118eb9997
Simone Celia <simone.celia@simonecelia.it>
2025-04-30 10:13:11 +0200

    1.0.3

--- src/main/java/it.simonecelia.ipapiclient/RestApiCaller.java
+++ src/main/java/it.simonecelia.ipapiclient/RestApiCaller.java
@@ -1,6 +1,8 @@
 package it.simonecelia.ipapiclient;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -39,8 +41,21 @@ public class RestApiCaller {
 	}
 
 	private static void sendPutRequest ( String endpoint, int minutes ) throws Exception {
-		var responseCode =  getHttpURLConnection ( endpoint ).getResponseCode ();
-		var message = String.format ( "%s Sent request to: %s, response code: %d, next call in %d minutes", now (), endpoint, responseCode, minutes );
+		var connection = getHttpURLConnection ( endpoint );
+		int responseCode = connection.getResponseCode ();
+
+		var response = new StringBuilder ();
+		try ( var reader = new BufferedReader (
+						new InputStreamReader ( responseCode >= 200 && responseCode < 300 ?
+										connection.getInputStream () : connection.getErrorStream () ) ) ) {
+			String line;
+			while ( ( line = reader.readLine () ) != null ) {
+				response.append ( line );
+			}
+		}
+
+		var message = String.format ( "%s Sent request to: %s, response code: %d, next call in %d minutes. %s",
+						now (), endpoint, responseCode, minutes, response );
 		System.out.println ( message );
 	}
 
